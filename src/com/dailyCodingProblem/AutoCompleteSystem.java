e2d3afa7aab41992f08af66bb3f42aa6969b1174
Kumar Mangalam <kumar.mangalam@datavalanalytics.com>
2019-07-30 00:32:44 +0530

    Auto Complete System solved

--- src/com/dailyCodingProblem/AutoCompleteSystem.java
+++ src/com/dailyCodingProblem/AutoCompleteSystem.java
@@ -1,5 +1,11 @@
 package com.dailyCodingProblem;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
 //TODO: This problem was asked by Twitter.
 /**
  * Implement an autocomplete system.
@@ -18,8 +24,25 @@ public class AutoCompleteSystem
 {
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Enter keyword to search: ");
+		String str = scn.nextLine();
+		Set<String> dict = new HashSet<>();
+		List<String> result = new ArrayList<>();
+		System.out.print("\nEnter Total Inputs in dictionary:");
+		int totalInputs = scn.nextInt();
+		scn.next();
+		for(int i=0;i<totalInputs;i++)
+		{
+			dict.add(scn.nextLine());
+		}
+		for (String string : dict)
+		{
+			if(string.startsWith(str))
+				result.add(string);
+		}
+		System.out.println("Output: "+result);
+		scn.close();
 	}
 
 }
---
+++ src/com/dailyCodingProblem/DistinctLongestSubstring.java
@@ -0,0 +1,21 @@
+package com.dailyCodingProblem;
+
+//TODO: This problem was asked by Amazon.
+
+/**
+ * Given an integer k and a string s, find the length of the 
+ * longest substring that contains at most k distinct characters.
+ * For example, 
+ * given s = "abcba" and k = 2, 
+ * 	the longest substring with k distinct characters is "bcb".
+ * given s = "abcba" and k = 3, 
+ * 	the longest substring with k distinct characters is ["abcb","bcba"]
+ */
+public class DistinctLongestSubstring
+{
+	public static void main(String[] args)
+	{
+
+	}
+
+}
