3b621b9e9a7401ff128260a8b83b364373b9b400
21 Yards <manguatwork@gmail.com>
2020-04-26 12:51:17 +0530

    Linear Search

--- src/com/geeksForGeeks/algorithms/search/LinearSearch.java
+++ src/com/geeksForGeeks/algorithms/search/LinearSearch.java
@@ -1,12 +1,19 @@
-package com.geeksForGeeks.algorithms;
+package com.geeksForGeeks.algorithms.search;
 
 public class LinearSearch
 {
-
+    // Time complexity: O(n)
     public static void main(String[] args)
     {
         int[] arr = { 10, 20, 80, 30, 60, 50, 110, 100, 130, 170 };
         int numberToSearch = 110;
+        int elementIndex = linearSearch(arr, numberToSearch);
+        System.out.println(elementIndex);
+
+    }
+
+    private static int linearSearch(int[] arr, int numberToSearch)
+    {
         int elementIndex = -1;
 
         for (int i = 0; i < arr.length; i++)
@@ -14,8 +21,7 @@ public class LinearSearch
             if (numberToSearch == arr[i])
                 elementIndex = i;
         }
-        System.out.println(elementIndex);
-
+        return elementIndex;
     }
 
 }
